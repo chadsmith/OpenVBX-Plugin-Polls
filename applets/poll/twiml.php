5ab2b3cd8985187d2448299e93444e31455344c8
Chad Smith <chad@nospam.me>
2012-07-11 15:49:16 -0500

    Record correct number on outgoing calls

--- applets/poll/twiml.php
+++ applets/poll/twiml.php
@@ -2,9 +2,11 @@
 $ci =& get_instance();
 $poll = AppletInstance::getValue('poll');
 $option = AppletInstance::getValue('option');
+$direction = 'inbound';
 
-if(!empty($_REQUEST['From'])) {
-	$number = normalize_phone_to_E164($_REQUEST['From']);
+if(!empty($_REQUEST['Direction'])) {
+  $direction = $_REQUEST['Direction'];
+	$number = normalize_phone_to_E164('inbound' == $direction ? $_REQUEST['From'] : $_REQUEST['To']);
 	$ci->db->delete('polls_responses', array('poll' => $poll, 'value' => $number));
 	$ci->db->insert('polls_responses', array(
 		'poll' => $poll,
