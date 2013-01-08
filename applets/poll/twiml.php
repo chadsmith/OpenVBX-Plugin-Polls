db4cca1f75e761a103f97cd2501a316230f89a96
Chad Smith <chad@nospam.me>
2013-01-08 16:20:19 -0600

    Fix for inbound messages

--- applets/poll/twiml.php
+++ applets/poll/twiml.php
@@ -2,10 +2,9 @@
 $ci =& get_instance();
 $poll = AppletInstance::getValue('poll');
 $option = AppletInstance::getValue('option');
-$direction = 'inbound';
+$direction = isset($_REQUEST['Direction']) ? $_REQUEST['Direction'] : 'inbound';
 
-if(!empty($_REQUEST['Direction'])) {
-  $direction = $_REQUEST['Direction'];
+if(!empty($_REQUEST['From'])) {
 	$number = normalize_phone_to_E164(in_array($direction, array('inbound', 'incoming')) ? $_REQUEST['From'] : $_REQUEST['To']);
 	$ci->db->delete('polls_responses', array('poll' => $poll, 'value' => $number));
 	$ci->db->insert('polls_responses', array(
